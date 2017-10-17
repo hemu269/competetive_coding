f86aa1ce80480ef077521c5acc682f889373cb95
hemanth <hemanth9490324676@gmail.com>
2017-10-17 15:57:22 +0530

    hi

--- sorting/bubble_sort.cpp
+++ sorting/bubble_sort.cpp
@@ -4,7 +4,43 @@ using namespace std;
 
 int main()
 {
+	int n;
 
+	//cout<<"enter size of array\n";
+	cin>>n;   // size of input array
+
+	int array[n];
+
+	//cout<<"enter elements\n";
+
+	for (int i = 0; i < n; i++)
+	{
+		cin>>array[i];
+	}
+	int count,i,j;
+	for(i=0;i<n-1;i++)
+	{
+		count=0;
+		for(j=0;j<n-i-1;j++)
+		{
+			if(array[j]>array[j+1])
+			{
+				swap(array[j],array[j+1]);
+				count++;
+			}
+		}
+		if(count==0)
+		{
+			break;       // reduces best case complexity to O(n)
+		}
+	}
+
+	cout<<"sorted array is\n";
+
+	for(i=0;i<n;i++)
+	{
+		cout<<array[i]<<" ";
+	}
 
 	return 0;
 }
